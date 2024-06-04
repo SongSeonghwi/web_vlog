c1ab1e44905d65a5aa1189c3cf80c682e0ef08a8
SongSeonghwi <Sseonghwi@163.com>
2024-06-04 11:03:43 +0900

    ch18

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static'),
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 ALLOWED_HOST = ['124.0.0.1','pythonanywhere.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
